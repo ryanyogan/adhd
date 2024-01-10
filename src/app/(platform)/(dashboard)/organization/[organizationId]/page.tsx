0911d2e71f729091dbb2c8f3e9766c73e52b0500
Ryan Yogan <ryanyogan@gmail.com>
2024-01-10 17:46:22 -0600

    feat // first pass at sidebar on desktop

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,3 @@
 export default function OrganizationIdPage() {
-  return (
-    <div>
-      Congrats... you made it this far, now you have to build the fucking app
-    </div>
-  );
+  return <div></div>;
 }
