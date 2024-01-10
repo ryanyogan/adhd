cf27275e39173efc9e536d7680b121283c5eb977
Ryan Yogan <ryanyogan@gmail.com>
2024-01-10 10:37:11 -0600

    feat // welcome page complete

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,3 +1,6 @@
+import { Footer } from "./_components/footer";
+import { NavBar } from "./_components/nav-bar";
+
 export default function MarketingLayout({
   children,
 }: {
@@ -5,9 +8,11 @@ export default function MarketingLayout({
 }) {
   return (
     <div className="h-full bg-slate-100">
-      {/* Navbar */}
+      <NavBar />
+
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
-      {/* Footer */}
+
+      <Footer />
     </div>
   );
 }
