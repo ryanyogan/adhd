8b515639bdea2339c18113fbf698b49b050d7921
Ryan Yogan <ryanyogan@gmail.com>
2024-01-10 18:12:20 -0600

    feat // skeleton

--- src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -32,7 +32,7 @@ export function MobileSidebar() {
     <>
       <Button
         onClick={onOpen}
-        className="block md:hidden"
+        className="block md:hidden mr-2"
         variant="ghost"
         size="sm"
       >
--- src/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ src/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -103,3 +104,14 @@ export function NavItem({
     </AccordionItem>
   );
 }
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
